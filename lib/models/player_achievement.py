bed692261bfafdc12731aac70c599cf33de5f9ee
shirocodes <ishirapt@gmail.com>
2025-06-15 17:38:38 +0300

    Implemented achievements table and an association model(player_achievement

--- lib/models/player_achievement.py
+++ lib/models/player_achievement.py
@@ -1,9 +1,12 @@
 from .base import Base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey, Float
 
 class Player_achievement(Base):
     __tablename__ = 'player_achievements'
     
     id = Column(Integer(), primary_key=True)
     name = Column(String(), nullable=False, unique=True)
+    player_id = Column(Integer, ForeignKey('players.id'))
+    achievement_id = Column(Integer, ForeignKey('achievements.id'))
+    progress = Column(Float, default=0.0)
    
