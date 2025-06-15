7de80b5770394e949683d6651fc46b4b97c5f683
shirocodes <ishirapt@gmail.com>
2025-06-15 17:02:21 +0300

    Trade system: Enabled player-to-player trading with a trading proposal and acceptance system.

--- lib/models/trade.py
+++ lib/models/trade.py
@@ -1,9 +1,18 @@
 from .base import Base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey, JSON
+from sqlalchemy.orm import relationship
 
+# players can trade monsters, form connections/rival and compete
 class Trade(Base):
     __tablename__ = 'trades'
     
     id = Column(Integer(), primary_key=True)
-    name = Column(String(), nullable=False, unique=True)
+    from_playerID = Column(Integer(), ForeignKey('players.id'))
+    to_playerID = Column(Integer(), ForeignKey('players.id'))
+    offered_monsters = Column(JSON)
+    requested_monsters = Column(JSON)
+    status = Column(String, default="pending")
+    
+    from_player = relationship("player", foreign_keys=[from_playerID])
+    to_player = relationship("player", foreign_keys=[to_playerID])
    
